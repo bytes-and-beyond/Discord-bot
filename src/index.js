0032bb262ac5617cc15a986d11c407a2de70f352
Hadi Mchawrab <him06@mail.aub.edu>
2024-09-11 11:08:28 +0300

    Refactor import statements and fix formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const { EmbedBuilder } = require('discord.js'); 
-const fs = require('fs/promises'); // Import a way to store constants in files
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs/promises"); // Import a way to store constants in files
 const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
@@ -21,76 +21,68 @@ client.on("messageCreate", (message) => {
 
   if (message.content === "hello") {
     message.reply(`Hello ${message.author.displayName}, how can I help you?`); // fetch the user's display name
-  }
-  else if (message.content === "keefak") {
+  } else if (message.content === "keefak") {
     message.reply("Great, What about you?");
   }
 });
 
-
-
 let startTime;
 
-const loadStartTime = async () => { // in this function we will load the startTime from the file that we saved in the saveStartTime function
+const loadStartTime = async () => {
+  // in this function we will load the startTime from the file that we saved in the saveStartTime function
   try {
-    const data = await fs.readFile('startTime.txt', 'utf-8'); 
+    const data = await fs.readFile("startTime.txt", "utf-8");
     startTime = parseInt(data, 10); // Convert string back to number
   } catch (error) {
-    console.error('Error loading startTime:', error);
+    console.error("Error loading startTime:", error);
   }
 };
 
-const saveStartTime = async (newStartTime) => { // save the value of startTime to a file
+const saveStartTime = async (newStartTime) => {
+  // save the value of startTime to a file
   try {
-    await fs.writeFile('startTime.txt', newStartTime.toString());
+    await fs.writeFile("startTime.txt", newStartTime.toString());
   } catch (error) {
-    console.error('Error saving startTime:', error);
+    console.error("Error saving startTime:", error);
   }
 };
 
-
-
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
   const Today = new Date();
   switch (interaction.commandName) {
-    case 'start-meeting':
+    case "start-meeting":
       startTime = Date.now();
       await saveStartTime(startTime); // Save startTime to file to not lose it when another instance of the bot starts
-      interaction.reply('Meeting just started!');
+      interaction.reply("Meeting just started!");
       break;
 
-    case 'end-meeting':
+    case "end-meeting":
       if (!startTime) {
-        interaction.reply('No meeting in progress!');
+        interaction.reply("No meeting in progress!");
         return;
       }
       const endTime = Date.now();
       const duration = (endTime - startTime) / 1000;
-      const minutes = Math.floor(duration / 60);   
-      const formattedDate = `${Today.getDate()}/${Today.getMonth() + 1}/${Today.getFullYear()}`;
-      const formattedTime = `${Today.getHours()}:${Today.getMinutes() < 10 ? '0' : ''}${Today.getMinutes()}`;
-    
-      const meetingEmbed = new EmbedBuilder()
-        .setColor(0x0099ff) 
-        .setTitle('**Meeting Summary**')
-        
-        .addFields(
-          { name: '📅 Date:', value: formattedDate, inline: true },
-          { name: '⏲️ Time:', value: formattedTime, inline: true },
-          { name: '⌚ Duration:', value: `${minutes} min`, inline: false },
-          { name: '📍 Location:', value: 'Voice Channel', inline: false }
-        );
-  
-      interaction.reply({ embeds: [meetingEmbed] });
+      const minutes = Math.floor(duration / 60);
+      const formattedDate = `${Today.getDate()}/${
+        Today.getMonth() + 1
+      }/${Today.getFullYear()}`;
+      const formattedTime = `${Today.getHours()}:${
+        Today.getMinutes() < 10 ? "0" : ""
+      }${Today.getMinutes()}`;
+
+      ;
+      interaction.reply(`# **Meeting Summary**
+        📅 **Date:** ${formattedDate}
+        ⏲️ **Time:** ${formattedTime}
+        ⌚ **Duration:** ${minutes} min
+        📍 **Location:** Voice Channel`);
       startTime = null;
       break;
   }
 });
 
-
 loadStartTime(); // Load startTime from file on bot startup
 
-
 client.login(process.env.TOKEN);
-    
